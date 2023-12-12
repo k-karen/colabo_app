96450927a6d94fd092448879e6ecbf3a55f4313d
k-karen <k-karen@users.noreply.github.com>
2023-12-12 21:35:58 +0900

    first commit, base rails tutorial ch14, add docker-compose file

---
+++ dockers/db/docker-entrypoint-initdb.d/init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS `colabo_app_development`;
+GRANT ALL ON colabo_app_development.* TO 'colabo_app_user'@'%';
+
+CREATE DATABASE IF NOT EXISTS `colabo_app_test`;
+GRANT ALL ON colabo_app_test.* TO 'colabo_app_user'@'%';
